854ef12c1f7712ecc1f2263d16b358d2c258f6fd
Stuart Chen <stuartchen.dev@gmail.com>
2026-01-20 10:09:38 +0800

    refactor: add new Portfolio Structure folder and shift correct file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import ProjectsPage from "./components/ProjectsPage/ProjectsPage.jsx";
+import ProjectsPage from "./components/MainLayout/ProjectsPage/ProjectsPage.jsx";
 import './App.css';
+
+// Portfolio-Structure-NOTE:
+// <NavigationHeader />
+// <MainLayout>
+//   <AboutSection />
+//   <ProjectsPage />
+//   <ContactSection />
+// </MainLayout>
+// <Footer />
+
 
 
 function App() {
   return (
-      <ProjectsPage />
+      <>
+        <ProjectsPage />
+      </>
   )
 }
 
