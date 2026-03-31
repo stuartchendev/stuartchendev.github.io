1347c8ae16b1fdef802fa9bff3a07237a01270d9
Stuart Chen <stuartchen.dev@gmail.com>
2026-04-01 03:31:30 +0800

    docs(resume): resume update, complete the v1.6 portfolio

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,4 @@ export const DEFAULT_LANGUAGE: LanguageId = 'en'; // en | zh-Tw | jp
 
 export const MAX_TAGS = 3; // project tags maximum display number
 
-export const RESUME_URL = `${import.meta.env.BASE_URL}resume.pdf`
+export const RESUME_URL = `${import.meta.env.BASE_URL}Stuart_Chen_Frontend_Engineer_resume.pdf`
